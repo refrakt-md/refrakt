8e44219036f83911ba4911cb44a8e950ab135363
Björn Andersson <bjorn.olof.andersson@gmail.com>
2026-02-11 22:00:36 +0100

    migrated theme to use tailwind 4

--- packages/create-refract/template/vite.config.ts
+++ packages/create-refract/template/vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { refract } from '@refract-md/sveltekit';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), refract()],
+	plugins: [tailwindcss(), sveltekit(), refract()],
 });
